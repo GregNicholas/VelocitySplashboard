98358aeea8f1c3983aa236367388399fbd2d2a64
GregNicholas <59461870+GregNicholas@users.noreply.github.com>
2022-03-26 15:19:03 -0700

    format for desktop full image size

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ fetch(
     document.getElementById(
       "bg-author"
     ).textContent = `background: ${data.user.name}`;
-    document.body.style.backgroundImage = `url(${data.urls.small})`;
+    document.body.style.backgroundImage = `url(${data.urls.regular})`;
   })
   .catch((err) => {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1544084944-15269ec7b5a0?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDgwOTk4Nzg&ixlib=rb-1.2.1&q=85)`;
